305ee44ef20d76ba9ef060958122a30ec3a1372c
ArtemPopov18 <Arshawin1991gmail.com>
2022-07-15 21:40:41 +0700

    Add binding.

--- app/src/main/java/com/popov/weatherkotlin/MainActivity.kt
+++ app/src/main/java/com/popov/weatherkotlin/MainActivity.kt
@@ -2,10 +2,14 @@ package com.popov.weatherkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.popov.weatherkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
